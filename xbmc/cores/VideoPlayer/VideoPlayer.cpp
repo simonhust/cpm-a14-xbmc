4ab250aecfdd3dafb3e8a1d5db038a24911019a6
simonhust <96456374+simonhust@users.noreply.github.com>
2025-10-30 16:50:51 +0800

    Update VideoPlayer.cpp

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3991,14 +3991,14 @@ bool CVideoPlayer::OpenVideoStream(CDVDStreamInfo& hint, bool reset)
 
   SetAVChange("OpenVideoStream");
   
-  if (m_pInputStream && m_pInputStream->GetStreamType() == DVDSTREAM_TYPE_BLURAY)
+  if (m_pInputStream && m_pInputStream->IsStreamType(DVDSTREAM_TYPE_BLURAY))
   {
-      CDVDInputStreamBluray* blurayStream = dynamic_cast<CDVDInputStreamBluray*>(m_pInputStream);
-      if (blurayStream && blurayStream->IsResuming())
-      {
-          SetAudioStream(1);
-          CLog::Log(LOGDEBUG, "CVideoPlayer::OpenVideoStream - use the first audiotrack as default",);
-      }
+    CDVDInputStreamBluray* blurayStream = dynamic_cast<CDVDInputStreamBluray*>(m_pInputStream.get());
+    if (blurayStream && blurayStream->IsResuming())
+    {
+      SetAudioStream(1);
+      CLog::Log(LOGDEBUG, "CVideoPlayer::OpenVideoStream - use the first audiotrack as default");
+    }
   }
     
   return true;
